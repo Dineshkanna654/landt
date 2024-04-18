1bfc15745fb690b22c40510c19ddd32b917834bb
diensh-kanna <dineshkanna@squapl.com>
2024-04-19 00:31:26 +0530

    Update App.tsx to use react-router-dom v6 and add Login page
    In this update, the App.tsx file has been modified to use the new version 6 of react-router-dom. The new version

---
+++ src/pages/login.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { Button, Checkbox, Form, Input, Typography } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+import './login.css';
+
+const { Text, Title, Link } = Typography;
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const onFinish = async (values: any) => {
+        setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8000/login/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(values)
+        });
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+  
+        const data = await response.json();
+        console.log('API Response:', data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+  return (
+    <section className="login-section">
+      <div className="login-container">
+        <div className="login-header">
+        <svg
+            width="25"
+            height="24"
+            viewBox="0 0 25 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect x="0.464294" width="24" height="24" rx="4.8" fill="#1890FF" />
+            <path
+              d="M14.8643 3.6001H20.8643V9.6001H14.8643V3.6001Z"
+              fill="white"
+            />
+            <path
+              d="M10.0643 9.6001H14.8643V14.4001H10.0643V9.6001Z"
+              fill="white"
+            />
+            <path
+              d="M4.06427 13.2001H11.2643V20.4001H4.06427V13.2001Z"
+              fill="white"
+            />
+          </svg>
+
+          <Title level={2}>Login</Title>
+          <Text className="hello">
+            Welcome back to L&T!
+          </Text>
+        </div>
+        <Form
+          name="normal_login"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          layout="vertical"
+          requiredMark="optional"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item style={{ marginBottom: "0px" }}>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
