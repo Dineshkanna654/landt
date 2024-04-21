5de267a1298c865ed14f94f094407283f93e0f6c
diensh-kanna <dineshkanna@squapl.com>
2024-04-21 15:03:29 +0530

    "Import Navigate from react-router-dom and add a default route to login"
    Explanation:
    
    * The code importing `Navigate` from 'react-router-dom' is added.
    * A new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Login from './pages/login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom'; // Import Routes
 
 function App() {
   return (
     <div className="App">
       <Router> 
-        <Routes> {/* Wrap Routes around Route components */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/dashboard" element={<div>welcome to dashboard</div>}/> 
         </Routes>
