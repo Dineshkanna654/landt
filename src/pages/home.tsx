d6d71d640c49c03e6e95e4aab8688dfbd57adf32
diensh-kanna <dineshkanna@squapl.com>
2024-04-22 13:32:04 +0530

    home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from '../components/header';
 import './home.css';
+import StockIndication from '../components/stockIndication';
+import { Button } from 'antd';
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
         <Header/>
+
+        <div className='paper'>
+          <div className='stock-indication'>
+              <StockIndication/>
+          </div>
+
+          <div className='start-stop'>
+              <Button type='primary' className='start'>Start</Button>
+              <Button type='primary' className='stop'>Stop</Button>
+          </div>
+        </div>
     </div>
   );
 }
