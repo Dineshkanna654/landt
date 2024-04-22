ee2442a1ecfb318644a5a5056e5ba7525516bfdd
diensh-kanna <dineshkanna@squapl.com>
2024-04-22 13:47:49 +0530

    Update padding in components and add camera component to home page
    The padding property has been commented out in the following components:
    - live-camera
    - stock-indication
    - submit-btn
    
    The width property has been updated for

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,22 +3,27 @@ import Header from '../components/header';
 import './home.css';
 import StockIndication from '../components/stockIndication';
 import { Button } from 'antd';
+import Camera from '../components/camera';
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-        <Header/>
+      <Header />
 
-        <div className='paper'>
-          <div className='stock-indication'>
-              <StockIndication/>
-          </div>
-
-          <div className='start-stop'>
-              <Button type='primary' className='start'>Start</Button>
-              <Button type='primary' className='stop'>Stop</Button>
-          </div>
+      <div className='paper'>
+        <div className='stock-indication'>
+          <StockIndication />
         </div>
+
+        <div className='start-stop'>
+          <Button type='primary' className='start'>Start</Button>
+          <Button type='primary' className='stop'>Stop</Button>
+        </div>
+
+        <div className='camera-table-search' id='cam-tab'>
+          <Camera />
+        </div>
+      </div>
     </div>
   );
 }
